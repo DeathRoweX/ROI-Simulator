dd826efdd64389b707100482bdc0ed8cf3bef19e
DeathRoweX <WillRowe@gmail.com>
2018-01-31 00:22:07 -0500

    Update roi.cpp

--- roi.cpp
+++ roi.cpp
@@ -1,3 +1,4 @@
+
 /*
 	ROI Class Declarations
 */
@@ -6,36 +7,43 @@
 #define ROI_CPP
 
 #include "roi.hpp"
+#include <numeric>
+#include <algorithm>
 
 ROI::ROI(){
 
 }
 
-
 double ROI::currentYearROI(){
 
 	// define vector of x trials
+	std::vector<double> v(numberOfTrials);
+
 	// fill with random values between -15% and 20%
+	std::generate(v.begin(), v.end(), [](){
+		return ((rand() % 20) - 15) / 100;
+	});
 
-	// calc mean	
+	// calc mean
+
+
 	// calc sigma
-}
 
+}
 
 double ROI::calcBalance(){
 
 }
 
-void setYears(unsigned int y){
+void ROI::setYears(unsigned int y){
 	years = y;
 }
 
-void setAmount(double a){
+void ROI::setAmount(double a){
 	initialAmount = a;
 }
 
-
-int ROI::simulate(){
+void ROI::simulate(){
 	// reset data
 	if(!(annualROI.empty() || mean.empty() || 
 			sigma.empty() || annualBalance.empty())){
@@ -45,22 +53,18 @@ int ROI::simulate(){
 		sigma.clear();
 	}
 
-	for(int i = 0; i < years; i++){
+	for(unsigned int i = 0; i < years; i++){
 
 		annualROI.push_back(currentYearROI());
-		annualBalanc(calcBalance();
+		annualBalance.push_back(calcBalance());
 
 	}
 }
-
 
 void ROI::display(){
 
 
 }
-
-
-
 
 
 #endif
